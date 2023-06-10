a395451908f965721f7aa5b0e7d3d31b77f5e2ae
shahnewaz tameem <shahnewaztamim@gmail.com>
2023-06-10 22:51:45 +0600

    bean custom repo using command line args:

---
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.customer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerConfiguration {
+    @Value("${app.userFakeCustomerRepo:false}")
+    private Boolean userFakeCustomerRepo;
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Hooray from commandline runner");
+        };
+    }
+
+    @Bean
+    CustomerRepo customerRepo() {
+        System.out.println("userFakeCustomerRepo = " + userFakeCustomerRepo);
+        return userFakeCustomerRepo ?
+                new CustomerFakeRepository() : new CustomerRepository();
+    }
+}
--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,13 +1,11 @@
 package com.example.demo.customer;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
 @Repository
-@Primary
 public class CustomerRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
